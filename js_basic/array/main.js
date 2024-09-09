1505f941adb120d062b947cdeb5e4e67853060da
reo0306 <reo19840306@gmail.com>
2024-09-10 07:07:31 +0900

    add call, event, array

---
+++ js_basic/array/main.js
@@ -0,0 +1,22 @@
+const arry = [10, 20, 30, 40];
+const newArry = [];
+
+for (let i = 0; i < arry.length; i++) {
+    const val = arry[i] * 2;
+    if (val > 50) {
+        newArry.push(arry[i] * 2)
+    }
+}
+
+console.log(newArry);
+
+const newArry2 = arry.map(val => val * 2).filter(val => val > 50);
+console.log(newArry2);
+
+// valは値, iは何番目, arryは配列自体
+const newArry3 = arry.map((val, i, arry) => {
+    console.log(val, i, arry);
+    return val * 3;
+});
+
+console.log(newArry3);
