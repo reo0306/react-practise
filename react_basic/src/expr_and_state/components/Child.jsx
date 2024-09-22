1e29ede0cb76b987414ba9da16b2a732b14066cc
reo0306 <reo19840306@gmail.com>
2024-09-23 06:34:23 +0900

    add expr

---
+++ react_basic/src/expr_and_state/components/Child.jsx
@@ -0,0 +1,23 @@
+/* POINT 式と文
+式：何らかの値を返すもの（変数に代入できるもの）
+文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
+*/
+
+import "./Child.css";
+
+const Child = () => {
+  const fn = () => 'hello';
+  const b = fn();
+  const a = true ? 'hello' : 'bye';
+  if(true) { 'hello' }
+  return (
+    <div className="component">
+      <h3>式と文</h3>
+      <h3>{a}</h3>
+      <h3>{b}</h3>
+      <h3>{true ? 'hello' : 'bye'}</h3>
+    </div>
+  );
+};
+
+export default Child;
