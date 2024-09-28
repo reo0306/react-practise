1e773733b1dce467483b6a3c3c633cd608ee7f32
reo0306 <reo19840306@gmail.com>
2024-09-29 07:18:14 +0900

    add control_and_form

---
+++ control_and_form/practice_filter/Example.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Profile from "./components/Profile";
+
+const persons = [
+  {
+    name: "Geo",
+    age: 18,
+    hobbies: ["sports", "music"],
+  },
+  {
+    name: "Tom",
+    age: 25,
+    hobbies: ["movie", "music"],
+  },
+  {
+    name: "Lisa",
+    age: 21,
+    hobbies: ["sports", "travel", "game"],
+  },
+];
+
+const Example = () => {
+  const [filterVal, setFilterVal] = useState("");
+
+  return (
+    <>
+      <input type="text" value={filterVal} onChange={(e) => setFilterVal(e.target.value)} />
+      <ul>
+        {persons
+          .filter(person => person.name.indexOf(filterVal) !== -1)
+          .map((person) => (
+          <li key={person.name}>
+            <Profile {...person} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Example;
---
+++ control_and_form/practice_list/Example.jsx
@@ -0,0 +1,35 @@
+import Profile from "./components/Profile";
+
+const Example = () => {
+  const profiles = [
+    {
+      name: "Geo",
+      age: "18",
+      hobbies: ["sports","music"],
+    },
+    {
+      name: "Tom",
+      age: "25",
+      hobbies: ["movie","music"]
+    },
+    {
+      name: "Lisa", 
+      age: "21",
+      hobbies: ["sports","travel","game"]
+    },
+  ];
+
+  return (
+    <>
+      <ul>
+        {profiles.map((profile) => 
+        <li key={profile.name}>
+          <Profile {...profile} />
+        </li>
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default Example;
