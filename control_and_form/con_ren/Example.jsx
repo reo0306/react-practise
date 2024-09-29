a7bb48c54084aa511e78dc4c382d347d8c44c469
reo0306 <reo19840306@gmail.com>
2024-09-30 06:55:58 +0900

    add con_ren, refactor

---
+++ control_and_form/con_ren/Example.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const Example = () => {
+  const animals = ["Dog", "Cat", null, "Rat"];
+
+  const [filterVal, setFilterVal] = useState("");
+
+  return (
+    <>
+      <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => setFilterVal(e.target.value)}
+      />
+      <ul>
+        {animals
+          .filter((animal) => {
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
+            return isMatch;
+          })
+          .map((animal) => {
+            return (
+              <li key={animal}>
+                {animal ?? "null,undefinedでした"}
+                {animal === "Dog" && "★"}
+              </li>
+            );
+            //return <li key={animal}>{animal === "Dog" ? animal + "★" : animal}</li>
+            //if (animal === "Dog") {
+              //return <li key={animal}>{animal}★</li>
+            //} else {
+              //return <li key={animal}>{animal}</li>
+            //}
+          })}
+      </ul>
+    </>
+  );
+};
+
+export default Example;
