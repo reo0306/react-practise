7a868aa4bcd32801366d8314baf5021f2c70a1ca
reo0306 <reo19840306@gmail.com>
2024-10-01 06:34:41 +0900

    add reminder

---
+++ control_and_form/reminder/components/List.jsx
@@ -0,0 +1,19 @@
+const List = ({todos}) => {
+    const complete = () => {
+
+    }
+    return (
+        <div>
+            {todos.map(todo => {
+                return (
+                    <div key={todo.id}>
+                        <button onClick={complete}>完了</button>
+                        <span>{todo.content}</span>
+                    </div>
+                )
+            })}
+        </div>
+    );
+};
+
+export default List;
---
+++ control_and_form/reminder/components/Todo.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import List from "./List";
+import Form from "./Form";
+
+const Todo = () => {
+  const todosList = [
+    {
+      id: 1,
+      content: "店予約する",
+    },
+    {
+      id: 2,
+      content: "卵買う",
+    },
+    {
+      id: 3,
+      content: "郵便出す",
+    },
+  ];
+
+  const [ todos, setTod0s ] = useState(todosList);
+
+  return (
+    <>
+    <List todos={todos} />
+    <Form />
+    </>
+  )
+};
+
+export default Todo;
