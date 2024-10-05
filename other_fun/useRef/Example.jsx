80cb4b6682be8bde4b5357883f6c886d117cd510
reo0306 <reo19840306@gmail.com>
2024-10-06 07:29:36 +0900

    add other_fun

---
+++ other_fun/useRef/Example.jsx
@@ -0,0 +1,28 @@
+import { useState, useRef } from "react";
+
+const Case1 = () => {
+  const [value, setValue] = useState("");
+  const inputRef = useRef();
+
+  //console.log(inputRef);
+
+  return (
+    <div>
+      <h3>ユースケース1</h3>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
+        インプット要素をフォーカスする
+      </button>
+    </div>
+  );
+};
+
+const Example = () => {
+  return (
+    <>
+      <Case1 />
+    </>
+  );
+};
+
+export default Example;
