7e5edb1e5026bf45043e6069e5df28f78296bbde
reo0306 <reo19840306@gmail.com>
2024-10-08 06:38:29 +0900

    add debugg

---
+++ debugging/check_err/Example.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const Example = () => {
+   const [count, setCount] = useState({ val: 0 });
+
+   const countUp = () => {
+     setCount((prevstate) => {
+       const newState = { ...prevstate }
+       newState.val += 1;
+       return newState;
+     });
+   };
+
+   return (
+      <>
+       <p>現在のカウント数: {count.val}</p>
+       <button onClick={countUp}>+</button>
+      </>
+   );
+};
+
+export default Example;
