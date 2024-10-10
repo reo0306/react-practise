8da04c0a27bdabd904689d54fe6cfb9392974a26
reo0306 <reo19840306@gmail.com>
2024-10-11 07:02:02 +0900

    add hooks

---
+++ hooks_p1/practice_reducer/Example.jsx
@@ -0,0 +1,71 @@
+import { useReducer } from "react";
+
+const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case 'change':
+      return {...state, [payload.name]: payload.value };
+    case 'add':
+      return {...state, result: state.a + state.b };
+    case 'minus':
+      return {...state, result: state.a - state.b };
+    case 'divide':
+      return {...state, result: state.a / state.b };
+    case 'multiply':
+      return {...state, result: state.a * state.b };
+    default:
+      throw new Error('不明なタイプです');
+  }
+}
+
+const Example = () => {
+  const initState = {
+    a: 1,
+    b: 2,
+    result: 3,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  const calculate = (e) => {
+    dispatch({ type: e.target.value });
+  };
+
+  const numChangeHandler = (e) => {
+    dispatch({ type: 'change', payload: { name: e.target.name, value: parseInt(e.target.value) }});
+  }
+
+  return (
+    <>
+      <div>
+        a:
+        <input
+          type="number"
+          name="a"
+          value={state.a}
+          onChange={numChangeHandler}
+        />
+      </div>
+      <div>
+        b:
+        <input
+          type="number"
+          name="b"
+          value={state.b}
+          onChange={numChangeHandler}
+        />
+      </div>
+      <select value={state.type} onChange={calculate}>
+        {CALC_OPTIONS.map(type => {
+          return (
+            <option key={type} value={type}>{type}</option>
+          )
+        })}
+      </select>
+      <h1>結果：{state.result}</h1>
+    </>
+  );
+};
+
+export default Example;
