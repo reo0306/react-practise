049c18348fac31897f2152512d9d72ebf269d036
reo0306 <reo19840306@gmail.com>
2024-10-15 07:01:02 +0900

    add practice useContext

---
+++ hooks_p1/practice_useCon/start/Example.jsx
@@ -0,0 +1,19 @@
+import Input from "./components/Input";
+import Select from "./components/Select";
+import Result from "./components/Result";
+import { CalcProvider } from "./context/CalcContext";
+
+const Example = () => {
+  return (
+    <>
+      <CalcProvider>
+        <Input name="a"/>      
+        <Input name="b" />      
+        <Select />
+        <Result />      
+     </CalcProvider>
+    </>
+  );
+};
+
+export default Example;
