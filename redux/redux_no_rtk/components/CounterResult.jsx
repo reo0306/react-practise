df925b5c9449f28139db7697a36aabd3ea6ae31a
reo0306 <reo19840306@gmail.com>
2024-10-20 07:18:34 +0900

    add redux

---
+++ redux/redux_no_rtk/components/CounterResult.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+//import { useCounter } from "../context/CounterContext";
+
+const CounterResult = () => {
+  //const state = useCounter();
+  const state = useSelector(state => state);
+
+  return <h3>{state}</h3>;
+};
+
+export default CounterResult;
