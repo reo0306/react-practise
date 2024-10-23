5d59477add9362eae1f784d93d5020784b80df10
reo0306 <reo19840306@gmail.com>
2024-10-24 06:27:27 +0900

    add middleware

---
+++ redux/middleware/store/index.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from "./modules/counter";
+import logger from "./middeware/logger";
+
+export default configureStore({
+  reducer: {
+    counter: reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+  /*middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware();
+    console.log(middlewares)
+    const newMiddlewares = middlewares.concat(logger)
+    return newMiddlewares;
+  }*/
+});
---
+++ redux/middleware/store/middeware/logger.jsx
@@ -0,0 +1,13 @@
+// Redux Middleware
+const logger = (store) => {
+    return (next) => {
+      return (action) => {
+        console.log('変更前', action, store.getState());
+        next(action);
+        console.log('変更後', action, store.getState());
+        // storeはaction後の状態
+      };
+    }
+  }
+  
+  export default logger;
