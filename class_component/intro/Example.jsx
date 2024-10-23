b2a646ab8e8cf49f482d13223dbbf82ff5d3f15a
reo0306 <reo19840306@gmail.com>
2024-10-24 06:37:47 +0900

    add class component

---
+++ class_component/intro/Example.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Example extends Component {
+  render() {
+    return (
+      <h3>I'm Function Component!</h3>
+    );
+  }
+}
+/*const Example = () => {
+  return (
+    <h3>I'm Function Component</h3>
+  );
+};*/
+
+export default Example;
