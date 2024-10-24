12c0e1e532ce78b57250db3ccfbe0ccb58ff3f74
reo0306 <reo19840306@gmail.com>
2024-10-25 06:43:48 +0900

    add state, error boundary

---
+++ class_component/error_boundary/Example.jsx
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import BuggyCounter from "./component/BuggyCounter";
+import ErrorBoundary from "./component/ErrorBoundary";
+
+class Example extends Component {
+  render() {
+    return (
+      <div>
+        <h3>React16 Error Boundaries</h3>
+        <p>下記の数値をクリック！</p>
+        <hr />
+        <h4>同じError Boundary内のコンポーネント</h4>
+        <ErrorBoundary>
+          <BuggyCounter />
+          <BuggyCounter />
+        </ErrorBoundary>
+        <hr />
+        <h4>異なるError Boundary内のコンポーネント</h4>
+        <ErrorBoundary>
+          <BuggyCounter />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <BuggyCounter />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+
+export default Example;
---
+++ class_component/error_boundary/component/ErrorBoundary.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  //static getDerivedStateFromError(error){
+    // 副作用の記述NG
+  //}
+  
+  componentDidCatch(error, errorInfo) {
+    // 副作用の記述OK
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+  }
+
+  render() {
+    if (this.state.errorInfo) {
+      return (
+        <div>
+          <h3>エラー発生</h3>
+          <details style={{ whiteSpace: "pre-wrap" }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
