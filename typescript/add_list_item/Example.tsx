4bd2e0a1dce05baa3b1fc754218535bafb69baa7
reo0306 <reo19840306@gmail.com>
2024-11-13 07:04:18 +0900

    add event, state, todos

---
+++ typescript/add_list_item/Example.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import TaskList from "./TaskList";
+
+export type Todo = {
+    id: number,
+    text: string,
+};
+
+const Example = () => {
+   const [ inputText, setInputText ] = useState("");
+   const [ todos, setTodos ] = useState([
+        { id: 0, text: 'Todo1'},
+        { id: 1, text: 'Todo2'},
+    ]);
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value);
+
+    const addTodoItem = () => {
+        setTodos(state => [...state, { id: state.length, text: inputText }])
+        setInputText("")
+    };
+
+    return (
+        <>
+            <input type="text" value={inputText} onChange={changeHandler}/>
+            <button onClick={addTodoItem}>追加</button>
+            <TaskList todos={todos} />
+        </>
+    )
+};
+
+export default Example;
---
+++ typescript/add_list_item/TaskList.tsx
@@ -0,0 +1,17 @@
+import type { Todo } from "./Example";
+
+type TaskListProps = {
+    todos: Todo[],
+};
+
+const TaskList: React.FC<TaskListProps> = ({ todos }) => {
+    return (
+        <ul>
+            {todos.map(todo => {
+                return <li key={todo.id}>{todo.text}</li>
+            })}
+        </ul>
+    )
+};
+
+export default TaskList;
